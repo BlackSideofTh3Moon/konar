8f734bd839cb161e19abefb77dd54d7ac1971e95
BlackSideofTh3Moon <101659468+BlackSideofTh3Moon@users.noreply.github.com>
2023-05-16 12:07:49 +0200

    Add files via upload

--- KONAR/scripts/show_oferta.js
+++ KONAR/scripts/show_oferta.js
@@ -12,30 +12,67 @@ card.forEach(element =>{
     element.addEventListener("click", () =>{
         var elementId = element.getAttribute("id");
 
+        
+
        switch (elementId){
         case "tarcica":
-            if(descContainer.className === "active"){
+            /*
+                ---Sprawdzenie, czy kontener wraz z kartą
+                ---TARCICA SUCHA nie zawiera klasy ACTIVE
+
+                Jeśli żadedem element nie zawiera, przypisuje
+                kontenerowi klase ACTIVE.
+
+                Jeśli jakiś element z nich zawiera, zwraca pustą
+                klase kontenerowi.
+            */
+            if(descContainer.className === "" && tarcicaCard.className === "card"){
+                descContainer.classList.add("active");
+            } else {
                 descContainer.className = "";
-            } 
+            }
+
+            /*
+                ---Sprawdzenie, czy karta tylko TARCICA 
+                ---SUCHA nie zawiera klasy ACTIVE
+
+                Jeśli jej nie zawiera, to ją dodaje a innym
+                kartom z produktami usuwa, a kontenerowi przypisuje
+                klase ACTIVE po 1 sekundzie, aby animacja zamykania się
+                kontenera zdązyła się wykonać
+            */
+
             if(tarcicaCard.className === "card"){
                 tarcicaCard.classList.add("active");
                 okleinyCard.className = "card";
                 elMebloweCard.className = "card";
                 podlogiCard.className = "card";
-                setTimeout(() => {
-                    descContainer.classList.add("active");
-                }, 1000);
-                
-                    
-                
-            } else {
+                if(descContainer.className === ""){
+                    setTimeout(() => {
+                        descContainer.classList.add("active");
+                    }, 1000);
+                }
+            } 
+            
+            /*
+                ---Jeśli karta TARCICA SUCHA zawiera klase
+                ---ACTIVE, to zostaje ona usunięta, wraz z 
+                ---usunięciem klasy ACTIVE z kontenera 
+            */
+            
+            else {
                 tarcicaCard.className = "card";
                 descContainer.className = "";
             }
             break;
        
-       case "okleiny":
-        descContainer.className = "";
+        case "okleiny":
+            if(descContainer.className === "" && okleinyCard.className === "card"){
+                descContainer.classList.add("active");
+            } else {
+                descContainer.className = "";
+            }
+
             if(okleinyCard.className === "card"){
                 okleinyCard.classList.add("active");
                 tarcicaCard.className = "card";
@@ -53,7 +90,12 @@ card.forEach(element =>{
             break;
 
         case "el_meblowe":
-            descContainer.className = "";
+            if(descContainer.className === "" && elMebloweCard.className === "card"){
+                descContainer.classList.add("active");
+            } else {
+                descContainer.className = "";
+            }
+
             if(elMebloweCard.className === "card"){
                 elMebloweCard.classList.add("active");
                 okleinyCard.className = "card";
@@ -71,7 +113,12 @@ card.forEach(element =>{
             break;
 
         case "podlogi":
-            descContainer.className = "";
+            if(descContainer.className === "" && podlogiCard.className === "card"){
+                descContainer.classList.add("active");
+            } else {
+                descContainer.className = "";
+            }
+            
             if(podlogiCard.className === "card"){
                 podlogiCard.classList.add("active");
                 okleinyCard.className = "card";
@@ -88,10 +135,8 @@ card.forEach(element =>{
             }
             break;
        }
-        
-        
-    })
-})
+    });
+});
 
 
 
