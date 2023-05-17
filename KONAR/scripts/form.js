3dbb5c5e3dc2eca962349839f1cd0f4281148839
BlackSideofTh3Moon <101659468+BlackSideofTh3Moon@users.noreply.github.com>
2023-05-17 16:40:29 +0200

    Add files via upload

---
+++ KONAR/scripts/form.js
@@ -0,0 +1,23 @@
+function sendMail(){
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+    const serviceID = "service_1uqzuxx";
+    const templateID = "template_fclclpr";
+
+    emailjs
+        .send(serviceID,templateID,params)
+        .then((res) =>{
+            
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("message").value = "";
+
+                console.log(res);
+                alert("Wysłano wiadomość");
+            })
+        .catch((err)=>console.log(err));
+}
+
