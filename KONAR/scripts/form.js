0c260c0c804d8bb594d81489e613affd4238647e
BlackSideofTh3Moon <101659468+BlackSideofTh3Moon@users.noreply.github.com>
2023-05-19 14:15:48 +0200

    Add files via upload

--- KONAR/scripts/form.js
+++ KONAR/scripts/form.js
@@ -1,23 +1,70 @@
-document.getElementById("form_btn").addEventListener("click", () =>{
+function sendForm(){
+    function validEmail(email){
+        var validEmail = String(email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+
+        if(validEmail === null){
+            return null;
+        }
+        else{
+            return validEmail[0];
+        }
+        
+    }
+    
     var params = {
         name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
+        email: validEmail(document.getElementById("email").value),
         message: document.getElementById("message").value,
     };
+
     const serviceID = "service_1uqzuxx";
     const templateID = "template_fclclpr";
 
-    emailjs
-        .send(serviceID,templateID,params)
-        .then((res) =>{
-            
-                document.getElementById("name").value = "";
-                document.getElementById("email").value = "";
-                document.getElementById("message").value = "";
+    var result = document.getElementById("form_result"); // Blok z wynikiem
+    if(params.name === "" || params.email === null){
+        result.textContent = "Wypełnij dane prawidłowo!";
+        result.style.display = "grid";
+        result.style.color = "red";
+    }
+   
+    else{
+            let btnbg = document.getElementById("form_btn_bg")
+            let btn = document.getElementById("form_btn");
+            btn.removeAttribute('onclick', ''); 
+              
+        
+     
 
-                console.log(res);
-                alert("Wysłano wiadomość");
-            })
-        .catch((err)=>console.log(err));
-});
+        emailjs
+            .send(serviceID,templateID,params)
+            .then((res) =>{
+                
+                    document.getElementById("name").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("message").value = "";
+
+                    console.log(res);
+
+                    // Informacja odnośnie wysłąnia formularza
+                    btn.querySelector('a').textContent ="Wysłano!";
+                    btnbg.style.height = "48px";
+                    btn.style.color = "var(--primary-white)";
+                    
+                    setTimeout(() => {
+                        btn.setAttribute('onclick', 'sendForm()');
+                        btn.querySelector('a').textContent ="Wyślij";
+                        btnbg.style.height = "4px";
+                        btn.querySelector('a').style.color = "var(--primary-dark)";
+                    }, 10000);     
+                })
+            .catch((err)=>console.log(err));
+           
+          
+    }
+  
+}
 
